fea65eff020b0d1525343a26058100c59e827064
NikitaMalik2303 <nikitamalik2303@gmail.com>
2022-07-18 17:08:38 +0530

    arrays done

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
 
+const gamePattern = [];
+
+const buttonColors = [ "red", "green" , "yellow" , "blue" ];
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColor = buttonColors[randomNumber];
+
+}
+
+$("."+randomChosenColor).fadeOut(100).fadeIn(100);
