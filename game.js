9fdd60bc9052cf3b1a8ccdcb9cc5397b47fbb437
NikitaMalik2303 <nikitamalik2303@gmail.com>
2022-07-19 14:54:19 +0530

    animation added

--- game.js
+++ game.js
@@ -1,12 +1,26 @@
 
 const gamePattern = [];
 
+const userClickPattern = [];
+
 const buttonColors = [ "red", "green" , "yellow" , "blue" ];
 
+$(".btn").click(function(){
+
+  var userChosenColor = $(this).attr("id") ;
+  userClickPattern.push(userChosenColor);
+
+})
+
 function nextSequence(){
+
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeIn(100);
+  var audiostring = "sounds/"+randomChosenColor+".mp3";
+  var audio = new audio(audiostring);
+  audio.play();
+
 
 }
-
-$("."+randomChosenColor).fadeOut(100).fadeIn(100);
